14af75a79f3dbd61e572a4088f29007c794ac920
Marwan2077 <98744657+yehiaeljulany@users.noreply.github.com>
2025-03-06 01:33:51 +0200

    Update Assignment5.sol

--- src/Assignment5.sol
+++ src/Assignment5.sol
@@ -3,19 +3,31 @@ pragma solidity ^0.8.0;
 
 contract Assignment5 {
     // 1. Declare a public unsigned integer variable called `counter`
+    uint public counter;
 
     // 2. Declare a public boolean variable called `isActive`
+    bool public isActive;
 
     // 3. Create a public mapping from an address to a string called `userNames`
+    mapping(address => string) public userNames;
 
     // 4. Define a struct `User` with fields: `id` (uint), `name` (string)
+    
+    struct User {
+        uint id;
+        string name;
+        address userAddress;
+    }
 
     // 5. Create a public array of `User` called `users`
+    User[] public users;
+
 
     // Constructor
     // Initialize `isActive` to true
     constructor() {
         // Fill in the logic
+        isActive = true;
 
     }
 
@@ -23,8 +35,13 @@ contract Assignment5 {
     // This function should:
     // - Be external
     // - Use a for loop to increment `counter` by 1 for 5 iterations
-    function incrementCounter() {
+    function incrementCounter() external {
         // Fill in the logic
+        for(uint i=0;i<5;i++) {
+
+            counter = counter +1;
+        }
+
       
     }
 
@@ -32,8 +49,15 @@ contract Assignment5 {
     // This function should:
     // - Be public
     // - Use an if statement to toggle `isActive` between true and false
-    function toggleActive() {
+    function toggleActive() public returns (string memory) {
         // Fill in the logic
+        if (isActive) {
+            isActive = false;
+            return "isActive set to false";
+        } else {
+            isActive = true;
+            return "isActive set to true";
+        }
         
     }
 
@@ -45,9 +69,10 @@ contract Assignment5 {
         // The memory keyword will be explained later
     // - Add a new user to the `users` array
     // - Update the `userNames` mapping with the address and name
-    function addUser() {
+    function addUser(uint id, string memory name , address userAddress )public {
         // Fill in the logic
-
+         users.push(User (id, name,userAddress));
+         userNames[userAddress] = name;
         // Hint: you have to use the keyword `memory` to define the struct
             // For example: User memory user
             // This keyword will be explained later
@@ -58,8 +83,9 @@ contract Assignment5 {
     // This function should:
     // - Be public and view
     // - Return the length of the `users` array
-    function getUserCount() {
+    function getUserCount()public view returns(uint) {
         // Fill in the logic
+        return users.length;
 
     }
 }
